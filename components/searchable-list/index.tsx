00cfae256e862f7322b1c455891091a09e8c6c96
Wouter van der Wal <33957974+wjtje@users.noreply.github.com>
2021-02-21 11:25:59 +0100

    ✨ Added a searchable list

--- components/searchable-list/index.tsx
+++ components/searchable-list/index.tsx
@@ -1,5 +1,14 @@
-import {List, ListItem, ListItemText} from '@material-ui/core'
-import React from 'react'
+import {createStyles, List, ListItem, ListItemText, makeStyles, TextField} from '@material-ui/core'
+import React, {useCallback, useEffect, useState} from 'react'
+
+// Custom styles used by this component
+const useStyle = makeStyles(theme =>
+	createStyles({
+		searchBar: {
+			marginTop: theme.spacing(1)
+		}
+	})
+)
 
 interface props<T> {
 	/**
@@ -38,46 +47,92 @@ interface props<T> {
  */
 // eslint-disable-next-line @typescript-eslint/comma-dangle
 const SearchableList = <T,>({list, listProperties, onClick, className}: props<T>) => {
+	const [searchBarText, setSearchBarText] = useState('')
+	const [displayList, setDisplayList] = useState<T[]>([])
+	const classes = useStyle()
+
+	/**
+	 * Get the primary text of an item
+	 */
+	const getPrimaryText = useCallback(
+		(item: T) => {
+			// Get the primaryText
+			if (typeof listProperties.primaryText === 'function') {
+				return listProperties.primaryText(item)
+			}
+
+			return String(item[listProperties.primaryText])
+		},
+		[listProperties]
+	)
+
+	/**
+	 * Get the secondary text of an item
+	 */
+	const getsecondaryText = useCallback(
+		(item: T) => {
+			// Get the primaryText
+			if (typeof listProperties.secondaryText === 'function') {
+				return listProperties.secondaryText(item)
+			}
+
+			return String(item[listProperties.secondaryText])
+		},
+		[listProperties]
+	)
+
+	useEffect(() => {
+		const searchText = searchBarText.toLowerCase()
+
+		setDisplayList(
+			list.filter(item => {
+				return (
+					getPrimaryText(item).toLowerCase().includes(searchText) ||
+					getsecondaryText(item).toLowerCase().includes(searchText)
+				)
+			})
+		)
+	}, [searchBarText, list, getPrimaryText, getsecondaryText])
+
 	return (
-		<List>
-			{list?.map(item => (
-				<ListItem
-					key={String(item[listProperties.key])}
-					button
-					onClick={() => {
-						onClick(item)
-					}}
-				>
-					<ListItemText
-						className={(() => {
-							// Get the classname
-							if (typeof className === 'function') {
-								return className(item)
-							}
+		<>
+			<TextField
+				fullWidth
+				className={classes.searchBar}
+				placeholder="Search here"
+				value={searchBarText}
+				onChange={event => {
+					setSearchBarText(event.target.value)
+				}}
+			/>
 
-							return ''
-						})()}
-						primary={(() => {
-							// Get the primaryText
-							if (typeof listProperties.primaryText === 'function') {
-								return listProperties.primaryText(item)
-							}
+			<List>
+				{displayList?.map(item => (
+					<ListItem
+						key={String(item[listProperties.key])}
+						button
+						onClick={() => {
+							onClick(item)
+						}}
+					>
+						<ListItemText
+							className={(() => {
+								// Get the classname
+								if (typeof className === 'function') {
+									return className(item)
+								}
 
-							return item[listProperties.primaryText]
-						})()}
-						secondary={(() => {
-							// Get the secondaryText
-							if (typeof listProperties.secondaryText === 'function') {
-								return listProperties.secondaryText(item)
-							}
-
-							return item[listProperties.secondaryText]
-						})()}
-					/>
-				</ListItem>
-			))}
-		</List>
+								return ''
+							})()}
+							primary={getPrimaryText(item)}
+							secondary={getsecondaryText(item)}
+						/>
+					</ListItem>
+				))}
+			</List>
+		</>
 	)
 }
 
 export default SearchableList
+
